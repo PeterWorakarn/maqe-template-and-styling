c3996135c1c06c5d31a932e7a277f2d218502ac3
Worakarn Oun <skboardgames@gmail.com>
2023-09-26 22:37:34 +0700

    Update ForumCard.tsx

--- src/ForumCard.tsx
+++ src/ForumCard.tsx
@@ -5,15 +5,15 @@ import dayjs from 'dayjs';
 type TFormCardProps = TPost;
 
 const ForumCard: React.FC<TFormCardProps> = (props) => {
-  const authorInfo: TAuthor = authorList.find(i => i.id === props.author_id)!
+  const authorInfo: TAuthor | undefined = authorList.find(i => i.id === props.author_id)
 
   return (
     <article className={`${props.id % 2 === 0 ? 'bg-highlight' : 'bg-white'} shadow`}>
       <div className='flex gap-1 items-center flex-wrap justify-start p-4 border-b-[1px] border-slate-300'>
         <img className='w-8 h-8 rounded-full'
-          src={authorInfo.avatar_url}
-          alt={authorInfo.name} />
-        <p className='text-accent font-medium'>{authorInfo.name}</p>
+          src={authorInfo?.avatar_url ?? 'https://randomuser.me/api/portraits/men/1.jpg'}
+          alt={authorInfo ? authorInfo.name : 'maqe alt image'} />
+        <p className={`${authorInfo ? 'text-accent' : 'text-gray-400'} font-medium`}>{authorInfo?.name ?? 'undefined user'}</p>
         <p className='text-label'>posted on {`${dayjs(props.created_at).format('dddd, MMMM D, YYYY, HH:mm')}`}</p>
       </div>
       <div className='grid grid-cols-6 p-4 gap-4'>
